7a7323e6dda3116451a2670c34611876e8db9694
Tomas Komora <xkomorat@stuba.sk>
2021-10-14 22:19:40 +0200

    feat : implement pgtbl

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,6 +126,15 @@ found:
     release(&p->lock);
     return 0;
   }
+  
+  // Allocate a usyscall.
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  
+  p->usyscall->pid = p->pid; 
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -164,6 +173,10 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
+  p->usyscall = 0;
+  
 }
 
 // Create a user page table for a given process,
@@ -195,6 +208,13 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
 
   return pagetable;
 }
@@ -206,6 +226,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
